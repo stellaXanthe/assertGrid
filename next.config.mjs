bfa899e241f595fb337637e28ec09e0300f14eca
stellaXanthe <gelynkeil.delacruaz@gmail.com>
2026-08-09 15:39:52 +0800

    nxtconfigmjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ["@sparticuz/chromium", "playwright-core"],
-};
-
-export default nextConfig; 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["playwright-core", "@sparticuz/chromium"],
-  },
+  serverExternalPackages: ["playwright-core", "@sparticuz/chromium"],
   outputFileTracingIncludes: {
     "/api/**/*": ["./node_modules/playwright-core/**"],
   },
